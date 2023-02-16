5a71b9d5cb6023e7fb71fea86edc6005905cbd8f
mahpranto <pranto@kludioasia.com>
2023-02-16 20:12:37 +0000

    HomeScreen

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -43,6 +43,21 @@ function HomeScreen({navigation}: {navigation: any}) {
             <Text onPress={() => navigation.navigate('Note')} style={[styles.section1_1, {borderColor: 'white'}]}>Notes</Text>
           </View>
 
+          <View style={styles.section2}>
+            <View style={[styles.section2_1, {backgroundColor: '#0FE38A', borderColor: '#0FE38A'}]}>
+                <Text style={styles.section2_1_1}>Income</Text>
+                <Text style={styles.section2_1_1}>10.00 $</Text>
+            </View>
+            <View style={[styles.section2_1, {backgroundColor: '#FD6868', borderColor: '#FD6868'}]}>
+                <Text style={[styles.section2_1_1, {color: 'white'}]}>Expense</Text>
+                <Text style={[styles.section2_1_1, {color: 'white'}]}>10.00 $</Text>
+            </View>
+            <View style={[styles.section2_1, {backgroundColor: '#FFFFFF', borderColor: '#FFFFFF'}]}>
+                <Text style={styles.section2_1_1}>Total</Text>
+                <Text style={styles.section2_1_1}>10.00 $</Text>
+            </View>
+          </View>
+
           <TouchableOpacity style={styles.fab} activeOpacity={0.8} onPress={() => navigation.navigate('Add')}>
             <MaterialIcons name="add" color="black" size={40} />
           </TouchableOpacity>
@@ -86,6 +101,30 @@ const styles = StyleSheet.create({
     padding: 3,
     textAlign: 'center',
     color: 'white',
+  },
+
+  section2: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: "space-between",
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 20,
+  },
+
+  section2_1: {
+    width: '30%',
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderRadius: 10,
+    padding: 5
+  },
+
+  section2_1_1: {
+    padding: 1,
+    textAlign: 'center',
+    fontSize: 15,
+    fontWeight: '500'
   },
 
   fab: {
