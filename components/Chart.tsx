2c1edcc6ad78ab8793f6fb9cf2076ec7f9f2f7d0
mahpranto <pranto@kludioasia.com>
2023-03-14 23:22:09 +0000

    Chart Updates

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -10,16 +10,40 @@ import {
     View,
     TextInput,
     FlatList,
+    Dimensions,
   } from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import uuid from 'react-native-uuid';
 import { useIsFocused } from '@react-navigation/native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+} from "react-native-chart-kit";
 
 function Chart({navigation}: {navigation: any}) {
 
     const isFocused = useIsFocused();
+
+    const data = [
+        {
+          name: "Income",
+          population: 21500000,
+          color: "rgba(131, 167, 234, 1)",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15
+        },
+        {
+          name: "Expense",
+          population: 2800000,
+          color: "#F00",
+          legendFontColor: "#7F7F7F",
+          legendFontSize: 15
+        }
+      ];
 
     useEffect(() => {
         
@@ -35,6 +59,73 @@ function Chart({navigation}: {navigation: any}) {
                     </View>
                 </View>
 
+                <LineChart
+                data={{
+                labels: ["Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"],
+                datasets: [
+                    {
+                    data: [
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                    ]
+                    }
+                ]
+                }}
+                width={Dimensions.get("window").width} // from react-native
+                height={220}
+                yAxisLabel="$"
+                yAxisSuffix="k"
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                backgroundColor: "#e26a00",
+                backgroundGradientFrom: "#fb8c00",
+                backgroundGradientTo: "#ffa726",
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                    
+                },
+                propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#ffa726"
+                }
+                }}
+                bezier
+                style={{
+                margin: 10,
+                borderRadius: 10,
+                }}
+            />
+
+        <PieChart
+        data={data}
+        width={Dimensions.get("window").width}
+        height={220}
+        chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#fb8c00",
+            backgroundGradientTo: "#ffa726",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            propsForDots: {
+                r: "6",
+                strokeWidth: "2",
+                stroke: "#ffa726"
+            }
+            }}
+        accessor={"population"}
+        backgroundColor={"transparent"}
+        paddingLeft={"0"}
+        />
+
             </SafeAreaView>
         
     );
@@ -45,7 +136,6 @@ const styles = StyleSheet.create({
     container: {
         marginLeft: 10,
         marginRight: 10,
-        flex: 1,
     },
 
     headSection: {
